31b98f2cfc4843373eb5e512cf4272ae29d48a68
thoriq <thoriq.noer@gmail.com>
2022-08-22 18:19:18 +0700

    feat: Integrasi admin page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\DashbboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')
+    ->group(function() {
+        Route::get('/', [DashbboardController::class, 'index']);
+});
+
+Auth::routes();

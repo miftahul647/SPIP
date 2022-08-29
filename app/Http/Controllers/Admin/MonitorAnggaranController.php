e26b4ed150f2dfca7f98df2431e881d0315561f5
thoriq <thoriq.noer@gmail.com>
2022-08-29 10:31:47 +0700

    add middleware auth

--- app/Http/Controllers/Admin/MonitorAnggaranController.php
+++ app/Http/Controllers/Admin/MonitorAnggaranController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class MonitorAnggaranController extends Controller
 {
+    // Midleware auth
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *

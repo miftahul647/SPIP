4bf03e1061046816c2308e3423d54e4efa29162f
thoriq <thoriq.noer@gmail.com>
2022-09-02 09:06:37 +0700

    feat: add feat dashboard monev kegiatan internal

---
+++ resources/views/pages/admin/kegiatan.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+@section('title')
+    Kegiatan Internal
+@endsection
+
+@section('content')
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Kegiatan Internal Jardik</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+
+        <div class="col-xl-12 col-md-12 col-sm-12">
+            <div class="frame-responsive">
+                <iframe id="idx_frame" src="https://www.appsheet.com/start/1fdcfcc4-efd8-47eb-aaf6-bf0b0a5ee84c" width="1400"
+                    height="2000" frameborder="0">
+                </iframe>
+            </div>
+        </div>
+
+    </div>
+@endsection

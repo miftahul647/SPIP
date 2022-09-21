89c11fb5cf2699067bc81c63546564ca718b8d04
thoriq <thoriq.noer@gmail.com>
2022-09-21 17:20:40 +0700

    tableau response

--- resources/views/pages/admin/monitoring/surat-tugas.blade.php
+++ resources/views/pages/admin/monitoring/surat-tugas.blade.php
@@ -9,11 +9,11 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard Surat Tugas</h1>
     </div>
-
+    {{ $tableau_response }}
     <div class="row">
         <div class="col-xl-12 col-md-6">
             <div class="frame-responsive">
-                <iframe id="idx_frame" src="{{ $tableau_response }}" width="1400" height="2000" frameborder="0">
+                <iframe id="idx_frame" src="" width="1400" height="2000" frameborder="0">
                 </iframe>
             </div>
         </div>

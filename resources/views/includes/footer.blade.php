3a1b228a8048dfa45221b36e8bb874a25251b466
thoriq <thoriq.noer@gmail.com>
2022-09-22 16:56:59 +0700

    fix: modified footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<section class="footer">
+<footer class="footer">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -9,4 +9,4 @@
             </div>
         </div>
     </div>
-</section>
+</footer>

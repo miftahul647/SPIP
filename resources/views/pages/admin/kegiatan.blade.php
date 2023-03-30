ea2bd8c79e243ebb167e81b6285414cb901028a0
thoriq.noer <thoriq.noer@gmail.com>
2023-03-30 10:32:28 +0700

    update: modified css page kegiatan and anggaran jardik

--- resources/views/pages/admin/kegiatan.blade.php
+++ resources/views/pages/admin/kegiatan.blade.php
@@ -6,22 +6,17 @@
 
 @section('content')
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Kegiatan Internal Jardik</h1>
-    </div>
 
     <!-- Content Row -->
     <div class="row">
 
         <div class="col-xl-12 col-md-12 col-sm-12">
-            <div class="frame-responsive" style="padding-bottom:56.25%; position:relative; display:block; width: 100%">
+            <div class="frame-responsive">
                 <iframe
-                    width="100%" 
-                    height="100%"
-                    id="idx_frame"
-                    src="https://www.appsheet.com/start/1fdcfcc4-efd8-47eb-aaf6-bf0b0a5ee84c" 
-                    frameborder="0"
-                    style="position:absolute; top:0; left: 0"
+                    class="responsive-iframe"
+                    src="https://www.appsheet.com/start/1fdcfcc4-efd8-47eb-aaf6-bf0b0a5ee84c"
+                    allowfullscreen
+                    flam
                     >
                 </iframe>
             </div>
--- resources/views/pages/admin/monitoring/anggaran-jardik.blade.php
+++ resources/views/pages/admin/monitoring/anggaran-jardik.blade.php
@@ -10,12 +10,33 @@
         <h1 class="h3 mb-0 text-gray-800">Dashboard Monitoring Anggaran Jardik</h1>
     </div>
     <div class="row">
-        <div class="col-xl-12 col-md-6">
-            <div class="frame-responsive">
-                <iframe id="idx_frame" src={{ $tableau_response }} scrolling="no" frameborder="0" style="height: 100%; overflow: hidden;">
-                </iframe>
+        <div class="col-md-12 col-md-6">
+            <div class="frame-responsive" style="height: 557vh;">
+                {{-- <iframe 
+                    id="idx_frame" 
+                    src={{ $tableau_response }} 
+                    scrolling="no" 
+                    frameborder="0" 
+                    allowfullscreen="true"
+                    style="display: block; width: 1200px; height: 827px; visibility: visible; overflow: hidden;">
+                </iframe> --}}
+                <div id="vizContainer"></div>
             </div>
         </div>
     </div>
     
 @endsection
+
+@push('footer_js')
+    <script src="{{ asset('vendor/tableau/tableau-2.9.1.min.js') }}"></script>
+    <script>
+        var data = {!! json_encode($tableau_response) !!}
+        var vizContainer = $("#vizContainer")
+
+        var createEl = document.createElement("div")
+        createEl.setAttribute("id", "vizIframe")
+        vizContainer.append(createEl)
+
+        new tableau.Viz(createEl, data)
+    </script>
+@endpush

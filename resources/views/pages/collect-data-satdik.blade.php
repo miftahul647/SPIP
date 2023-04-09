4718bfd928803f14588da7525c885be356b58af6
thoriq.noer <thoriq.noer@gmail.com>
2023-04-10 04:47:21 +0700

    create: Controller and page collect data satdik

---
+++ resources/views/pages/collect-data-satdik.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title')
+	Form Satuan Pendidikan
+@endsection
+
+@section('content')
+  <!-- Section sekolah -->
+  @component('components.title')
+      @slot('title')
+        Pengumpulan data satuan pendidikan
+      @endslot
+      <h6>
+        Form Data satuan pendidikan
+      </h6>
+  @endcomponent
+
+  
+  
+      
+@endsection

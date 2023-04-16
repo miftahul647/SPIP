7ecba52c204db339b6112b8ed1f656b8d227e2d3
thoriq.noer <thoriq.noer@gmail.com>
2023-04-17 02:57:40 +0700

    create: migration and model school

---
+++ database/migrations/2023_04_16_022111_create_schools_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->char('id', 4)->index();
+            $table->char('regency_id', 4);
+            $table->string('nama_sekolah', 100);
+            $table->foreign('regency_id')
+                ->references('id')
+                ->on('regencies')
+                ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+}

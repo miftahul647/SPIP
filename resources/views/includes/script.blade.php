bd77af56fb069745c0881d866324fc08155ee5be
thoriq.noer <thoriq.noer@gmail.com>
2023-05-13 15:00:10 +0700

    feat:tooltip button

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,4 +1,3 @@
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('vendor/jquery/jquery.js') }}"></script>
 <script src="{{ asset('vendor/owlcarousel/js/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('vendor/jquery/script.js') }}"></script>

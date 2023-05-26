78829dd310ed11038e4fb477655dc0b95e5a50aa
miftahul647 <miftahularialdi647@gmail.com>
2023-05-26 08:35:51 +0700

    Update FE Form and footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,8 +3,7 @@
         <div class="row">
             <div class="col-md-12">
                 <p>
-                    Hak Cipta © 2023 Komisi Pemberantasan Korupsi. Semua Hak
-                    Dilindungi.
+                    © 2023 Komisi Pemberantasan Korupsi. All Right Reserved.
                 </p>
             </div>
         </div>
